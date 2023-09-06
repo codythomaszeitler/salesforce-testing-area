d6a1c4e2f245c13d752a8e6df80db18a9a0d1258
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-09-05 20:37:25 -0700

    ya

--- force-app/main/default/lwc/testerLwc/testerLwc.js
+++ force-app/main/default/lwc/testerLwc/testerLwc.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from "lwc";
+import { TestClass } from "c/testClass";
 
-export default class TesterLwc extends LightningElement {}
+export default class TesterLwc extends LightningElement {
+  @api
+  recordId;
+
+  @track
+  testObject;
+
+  connectedCallback() {
+    this.testObject = new TestClass();
+  }
+}
